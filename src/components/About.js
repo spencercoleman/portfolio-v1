43043b89688b0d3e50bc2f7392a571281d7b7e78
spencercoleman <spencer@edc.vc>
2022-07-16 21:04:22 -0700

    Update About content; Update notations to animate when entering viewport

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,76 @@
-import React from "react";
+import React, { useRef } from 'react';
+import { useInViewport } from 'react-in-viewport';
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import styled from "styled-components";
 
 const AboutSection = styled.section`
     border-top: 1px solid;
     border-bottom: 1px solid;
     display: grid;
+    grid-template-columns: 2fr 1fr;
     gap: 25px;
+
+    .tech-container {
+        grid-column: 1 / 3;
+
+        @media (min-width: 600px) {
+            grid-column: 1 / 2;
+        }
+    }
+
+    .tech-list {
+        padding: 0;
+        margin: 0;
+        margin-block-end: 0;
+        margin-top: 10px;
+        list-style: none;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 10px;
+    
+        @media (min-width: 600px) {
+            gap: 25px;
+        }
+    }
 `;
 
 const About = () => {
+    const aboutRef= useRef(null);
+    const { enterCount } = useInViewport(aboutRef);
+
+    const technologies = ['JavaScript (ES6+),', 'Node.js,', 'React,', 'SQL,', 'Figma'];
+
     return (
-        <AboutSection>
-            <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <p>
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+        <AboutSection ref={aboutRef}>
+            <RoughNotationGroup show={enterCount}>
+                <p style={{gridColumn: '1 / 3'}}>
+                    I work on innovative projects that focus on {' '}
+                    <RoughNotation type="underline" color="var(--color-jelly)" strokeWidth={2} padding={1} iterations={1}>
+                        accessibile
+                    </RoughNotation>, {' '} 
+                    <RoughNotation type="underline" color="var(--color-dove)" strokeWidth={2} padding={1} iterations={1}>
+                        user-centered design
+                    </RoughNotation>. 
+                    My time is spent researching, prototyping, and developing features that help empower individuals and create enriching experiences.
+                </p>
+                <p style={{gridColumn: '1 / 3'}}>
+                    Outside of work, I'm usually {' '} 
+                    <RoughNotation type="underline" color="var(--color-paarl)" strokeWidth={2} padding={1} iterations={4}>
+                        catering to my cat's whims
+                    </RoughNotation>, practicing piano, playing niche indie games, or building personal projects. 
+                </p>
+                <div className="tech-container">
+                    <p>
+                        Lately, I have been working with:
+                    </p>
+                    <RoughNotation type="bracket" brackets={['left', 'right']} color="var(--color-jelly)" strokeWidth={2} padding={3}>
+                        <ul className="tech-list">
+                            {technologies.map((tech, i) => <li key={i}>{tech}</li>)}
+                        </ul>
+                    </RoughNotation>
+                </div>
+            </RoughNotationGroup>
+
         </AboutSection>
     );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useInViewport } from 'react-in-viewport';
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import styled from "styled-components";
 
@@ -7,6 +8,7 @@ const HeroSection = styled.section`
     flex-direction: column;
     justify-content: center;
     min-height: 100vh;
+    margin-bottom: 10px;
 
     .details {
         margin-top: 0;
@@ -21,21 +23,27 @@ const HeroSection = styled.section`
 `;
 
 const Hero = () => {
+    const heroRef= useRef(null);
+    const { enterCount } = useInViewport(heroRef);
+
     return (
-        <HeroSection>
+        <HeroSection ref={heroRef}>
             <h1>Hi, I'm Spencer.</h1>
             <h2>
-                <RoughNotation type="bracket" brackets={['left', 'right']} color="var(--color-dove)" strokeWidth={3} padding={3} show>
+                <RoughNotation type="bracket" brackets={['left', 'right']} color="var(--color-dove)" strokeWidth={3} padding={3} show={enterCount}>
                     Developer & Product Manager
                 </RoughNotation>
             </h2>
             <div className="details">
-                <RoughNotationGroup>
+                <RoughNotationGroup show={enterCount}>
                     <p>
-                        I'm on a mission to create <RoughNotation type="underline" color="var(--color-paarl)" strokeWidth={2} iterations={1} padding={2} multiline show>simple, intuitive, and delightful products</RoughNotation>.
+                        I'm on a mission to create <RoughNotation type="underline" color="var(--color-paarl)" strokeWidth={2} iterations={1} padding={2} multiline>simple, intuitive, and delightful products</RoughNotation>.
                     </p>
                     <p>
-                        Currently building solutions for global payment pain points at <RoughNotation type="underline" color="var(--color-jelly)" strokeWidth={2} iterations={3} show><a href="https://www.paymentlabs.io/" target="_blank" rel="noreferrer">Payment Labs</a></RoughNotation>. 
+                        Currently building tools to solve global payment pain points at {' '} 
+                        <RoughNotation type="underline" color="var(--color-jelly)" strokeWidth={2} iterations={3}>
+                            <a href="https://www.paymentlabs.io/" target="_blank" rel="noreferrer">Payment Labs</a>
+                        </RoughNotation>. 
                     </p>
                 </RoughNotationGroup>
             </div>
