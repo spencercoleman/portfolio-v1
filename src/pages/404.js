5691971cedc3add1ffb297f25aff6858e8b4389d
spencercoleman <spencer@edc.vc>
2022-07-18 23:57:24 -0700

    Fix package-lock; Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,37 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import GlobalStyle from "../styles/GlobalStyle";
+import { Link } from "gatsby";
+import { RoughNotation } from "react-rough-notation";
+import styled from "styled-components";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const NotFound = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    padding-bottom: 125px;
+    text-align: center;
+    p {
+      margin-top: 25px;
+    }
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+`;
 
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+    <NotFound>
+      <GlobalStyle />
+      <title>Page Not found</title>
+      <h1>404</h1>
+      <h2>There's nothing here</h2>
+      <p>
+        <Link to="/">
+          <RoughNotation type="box" color="var(--color-paarl)" strokeWidth={2} iterations={4} padding={15} show><strong>Go Home</strong></RoughNotation>
+        </Link>
       </p>
-    </main>
-  )
+    </NotFound>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
